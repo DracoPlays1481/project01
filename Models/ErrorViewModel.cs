11f0a7abc21e4f69ea23953efe4440af445163b8
DracoPlays1481 <42610817+DracoPlays1481@users.noreply.github.com>
2024-11-25 20:55:09 +0800

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EWDProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

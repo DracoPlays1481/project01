a5f36a532ece7dc9e34c10c3c92da5899240576f
Shawn Wong <42610817+DracoPlays1481@users.noreply.github.com>
2025-03-05 13:06:39 +0800

    Add files via upload

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ESD_PROJECT.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Add any additional user properties here
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
